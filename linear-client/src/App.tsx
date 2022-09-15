4b474da8948bdef67bb3114e6751ff178383ac95
Erlend Tyrmi <erty@tv2.dk>
2022-09-15 13:58:38 +0200

    force added .env file for local environment

--- linear-client/src/App.tsx
+++ linear-client/src/App.tsx
@@ -3,10 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const axios = require('axios').default;
+
+  let url = process.env.REACT_APP_API_BASE_URL
+
   return (
     <div className="App">
       <header className="App-header">
-        Det bliver godt
+        {url}
       </header>
     </div>
   );
