ae379d924a26ea09062cbb4f0996f16187fe2f65
Erlend Tyrmi <erlend@tyrmi.com>
2022-10-26 09:31:13 +0200

    cleaned up session panel

--- linear-client/src/menu/SessionMenu.tsx
+++ linear-client/src/menu/SessionMenu.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import { appText } from '../appText';
 import store from '../stores/store';
 import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout';
 import InboxIcon from '@mui/icons-material/Inbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { deepPurple } from '@mui/material/colors';
@@ -27,30 +28,42 @@ const SessionMenu = () => {
         });
     };
 
+    let name = store.session.user?.name ?? 'N N';
+
     return (
         <div>
             <Toolbar />
 
-            <Avatar alt="UserImage" src={require('../images/logo.png')} sx={{ width: 160, height: 160, bgcolor: deepPurple[500], marginLeft: '35px' }} />
+            {/* <Avatar alt="UserImage" src={require('../images/logo.png')} sx={{ width: 160, height: 160, bgcolor: deepPurple[500], marginLeft: '35px' }} /> */}
+            <Avatar alt={name} sx={{ width: 160, height: 160, bgcolor: deepPurple[500], marginLeft: '35px' }}>
+                {name}
+            </Avatar>
 
             <List>
-                {['BrugerNavn', 'email@mail.com', 'Log ud', 'Bureau (dropdown)'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                <ListItemButton onClick={handleLogout}>
+                <ListItem>
+                    <ListItemIcon>
+                        <PersonIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={store.session.user?.name} />
+                </ListItem>
+                <ListItem>
                     <ListItemIcon>
                         <MailIcon />
                     </ListItemIcon>
-                    <ListItemText primary={'Log out'} />
-                </ListItemButton>
+                    <ListItemText primary={store.session.user?.email} />
+                </ListItem>
+                <ListItem disablePadding></ListItem>
+            </List>
+            <Divider />
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton onClick={handleLogout}>
+                        <ListItemIcon>
+                            <LogoutIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={'Log out'} />
+                    </ListItemButton>
+                </ListItem>
             </List>
         </div>
     );
